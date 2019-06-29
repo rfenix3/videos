902e8ab4a550dab66246b8c9c3b76a01aa6418d7
rfenix3 <richard.fenix@gmail.com>
2019-06-28 20:20:37 -0700

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <p>Video App</p> 
+      </div>
+    )
+  }
+}
+
+export default App;
