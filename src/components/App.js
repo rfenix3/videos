cd6110da9906c846dac78a60a3df6a5a9aac2b76
rfenix3 <richard.fenix@gmail.com>
2019-06-29 20:06:06 -0700

    Refresh detailed video when new search item is submitted.

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,10 @@ class App extends React.Component {
       }
     })
     //console.log('response ', response);
-    this.setState({ videos: response.data.items})
+    this.setState({ 
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
   }
 
   onVideoSelect = (video) => {
